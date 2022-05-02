9b4dc541983f7e6a05b53e14ee826626b2dde8b6
Himanshu <himanshugoswami.12feb@gmail.com>
2022-05-02 22:32:25 +0530

    Put all unsused configs in one directory

--- init.lua
+++ init.lua
@@ -8,11 +8,11 @@ require "user.treesitter"
 require "user.lualine-config"
 require "user.toggleterm"
 require "user.alpha"
---require "user.nvimR"
 require "user.autopairs"
--- require "user.barbar-config"
 require "user.lsp"  -- Notice here we are using directory name
 -- If we use directory name then it'll automatically look for init.lua file in that directory
 
+--   NOT USED NOW
+-- require "user.nvimR"
 -- require "user.netrw"  --Using nnn as file explorer now
 
