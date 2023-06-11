f4bab6ae6fc43adb0e0c8864087a0e2f4b83ccb1
Himanshu <himanshugoswami.12feb@gmail.com>
2023-06-11 23:20:44 +0530

    Last commit from Manjaro

--- ftplugin/javascript.lua
+++
@@ -1,3 +0,0 @@
-vim.o.expandtab = true                        -- convert tabs to spaces
-vim.o.shiftwidth = 2                          -- the number of spaces inserted for each indentation
-vim.o.tabstop = 2                             -- insert 2 spaces for a tab
